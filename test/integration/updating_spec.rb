90e9839804add2f32c4d8817850a2c4d6db36b53
Cristi Balan <evil@che.lu>
2008-07-22 22:05:02 +0300

    add updating integration spec

---
+++ test/integration/updating_spec.rb
@@ -0,0 +1,87 @@
+require File.dirname(__FILE__) + '/../integration_helper'
+
+describe "Updating a mirror without conflicts" do
+
+  before do
+    FileUtils.rm_rf(TMP_PATH)
+    FileUtils.mkdir_p(TMP_PATH)
+  end
+
+  describe "from a git repository" do
+    before do
+      @shiny = create_git_repo_from_fixture("shiny")
+      @skit1 = create_git_repo_from_fixture("skit1")
+
+      in_dir(@shiny) do
+        `braid add --type git #{@skit1}`
+      end
+
+      update_dir_from_fixture("skit1", "skit1.1")
+      in_dir(@skit1) do
+        `git add *`
+        `git commit -m "change default color"`
+      end
+
+      update_dir_from_fixture("skit1", "skit1.2")
+      in_dir(@skit1) do
+        `git add *`
+        `git commit -m "add a happy note"`
+      end
+
+    end
+
+    it "should add the files and commit" do
+      in_dir(@shiny) do
+        `braid update skit1`
+      end
+
+      file_name = "layouts/layout.liquid"
+      output = `diff -U 3 #{File.join(FIXTURE_PATH, "skit1.2", file_name)} #{File.join(TMP_PATH, "shiny", "skit1", file_name)}`
+      $?.should.be.success
+
+      output = `git log --pretty=oneline`.split("\n")
+      output.length.should == 3
+      output[0].should =~ "Update mirror 'skit1/' to HEAD"
+    end
+
+  end
+
+  describe "from a svn repository" do
+    before do
+      @shiny = create_git_repo_from_fixture("shiny")
+      @skit1 = create_svn_repo_from_fixture("skit1")
+      @skit1_wc = File.join(TMP_PATH, "skit1_wc")
+
+      in_dir(@shiny) do
+        `braid add --type svn #{@skit1}`
+      end
+
+      update_dir_from_fixture("skit1_wc", "skit1.1")
+      in_dir(@skit1_wc) do
+        `svn commit -m "change default color"`
+      end
+
+      update_dir_from_fixture("skit1_wc", "skit1.2")
+      in_dir(@skit1_wc) do
+        `svn commit -m "add a happy note"`
+      end
+
+    end
+
+    it "should add the files and commit" do
+      in_dir(@shiny) do
+        `braid update skit1`
+      end
+
+      file_name = "layouts/layout.liquid"
+      output = `diff -U 3 #{File.join(FIXTURE_PATH, "skit1.2", file_name)} #{File.join(TMP_PATH, "shiny", "skit1", file_name)}`
+      $?.should.be.success
+
+      output = `git log --pretty=oneline`.split("\n")
+      output.length.should == 3
+      output[0].should =~ "Update mirror 'skit1/' to HEAD"
+    end
+
+  end
+
+end

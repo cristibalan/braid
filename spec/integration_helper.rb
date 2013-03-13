2243e39c176b10e84ba483ba3dd7d891055f21b4
Peter Donald <peter@realityforge.org>
2013-03-13 14:55:50 +1100

    Update tests to work on TravisCI

--- spec/integration_helper.rb
+++ spec/integration_helper.rb
@@ -35,6 +35,8 @@ def create_git_repo_from_fixture(fixture_name)
   update_dir_from_fixture(fixture_name)
 
   in_dir(git_repo) do
+    run_command("git config --global --get user.email || git config --global user.email \"you@example.com\"")
+    run_command("git config --global --get user.name  || git config --global user.name \"Your Name\"")
     run_command('git init')
     run_command('git add *')
     run_command("git commit -m \"initial commit of #{fixture_name}\"")

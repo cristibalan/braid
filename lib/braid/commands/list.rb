ae405edbe1783bfc461b670af82e98a294dfa03f
Peter Donald <peter@realityforge.org>
2013-03-15 14:12:51 +1100

    Improve the output of the list, making it easy to identify locally changed and remotely changes braids

--- lib/braid/commands/list.rb
+++ lib/braid/commands/list.rb
@@ -12,15 +12,16 @@ module Braid
         options.reject! { |k, v| %w(revision head).include?(k) }
         print "\n"
         msg "Listing all mirrors.\n=======================================================\n"
-        config.mirrors.each_with_index do |path, i|
+        config.mirrors.each do |path|
           mirror = config.get!(path)
-          print " #{i + 1}) #{path.to_s}"
+          print "#{path.to_s}"
           print " [LOCKED]" if mirror.locked?
           setup_remote(mirror)
           msg "Fetching new commits for '#{mirror.path}'." if verbose?
           mirror.fetch
           new_revision    = validate_new_revision(mirror, options["revision"])
-          print " !!! UPDATE AVAILABLE !!!" if new_revision.to_s != mirror.base_revision.to_s
+          print " (Remote Modified)" if new_revision.to_s != mirror.base_revision.to_s
+          print " (Locally Modified)" unless mirror.diff.empty?
           print "\n"
         end
         print "\n"

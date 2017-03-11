6ebbc4fb16cf1e38c5840188259497be9f4b93cd
Peter Donald <peter@realityforge.org>
2017-03-11 13:33:50 +1100

    Refactor the diff command so that if there is no parameters, diff occurs against every remote separating them by banners

--- lib/braid/commands/diff.rb
+++ lib/braid/commands/diff.rb
@@ -1,7 +1,26 @@
 module Braid
   module Commands
     class Diff < Command
-      def run(path, options = {})
+      def run(path = nil, options = {})
+        with_reset_on_error do
+          path ? diff_one(path, options) : diff_all(options)
+        end
+      end
+
+      protected
+
+       def diff_all(options = {})
+        print "\n"
+        msg "Diffing all mirrors.\n=======================================================\n"
+        config.mirrors.each do |path|
+          msg "Diffing #{path}\n=======================================================\n"
+          diff_one(path, options)
+          msg "=======================================================\n"
+        end
+        print "\n"
+      end
+
+      def diff_one(path, options = {})
         mirror = config.get!(path)
         setup_remote(mirror)
 

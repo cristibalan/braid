9d5614538cbb9c6f3f7b4e4289e88d9cfa57c28a
Peter Donald <peter@realityforge.org>
2017-03-11 13:25:00 +1100

    Add support for performing a status operation on a single remote

--- lib/braid/commands/list.rb
+++ lib/braid/commands/list.rb
@@ -10,29 +10,32 @@ module Braid
       protected
 
       def list_all(options = {})
-        options.reject! { |k, v| %w(revision head).include?(k) }
         print "\n"
         msg "Listing all mirrors.\n=======================================================\n"
         config.mirrors.each do |path|
-          mirror = config.get!(path)
-          setup_remote(mirror)
-          mirror.fetch
-          print path.to_s
-          print ' (' + mirror.base_revision + ')'
-          print ' [LOCKED]' if mirror.locked?
-          msg "Fetching new commits for '#{mirror.path}'." if verbose?
-          new_revision    = validate_new_revision(mirror, options['revision'])
-          print ' (Remote Modified)' if new_revision.to_s != mirror.base_revision.to_s
-          local_file_count = git.read_ls_files(mirror.path).split.size
-          if 0 == local_file_count
-            print ' (Removed Locally)'
-          elsif !mirror.diff.empty?
-            print ' (Locally Modified)'
-          end
-          print "\n"
-          clear_remote(mirror, options)
+          list_one(path, options)
         end
         print "\n"
+      end
+
+      def list_one(path, options = {})
+        mirror = config.get!(path)
+        setup_remote(mirror)
+        mirror.fetch
+        print path.to_s
+        print ' (' + mirror.base_revision + ')'
+        print ' [LOCKED]' if mirror.locked?
+        msg "Fetching new commits for '#{mirror.path}'." if verbose?
+        new_revision = validate_new_revision(mirror, options['revision'])
+        print ' (Remote Modified)' if new_revision.to_s != mirror.base_revision.to_s
+        local_file_count = git.read_ls_files(mirror.path).split.size
+        if 0 == local_file_count
+          print ' (Removed Locally)'
+        elsif !mirror.diff.empty?
+          print ' (Locally Modified)'
+        end
+        print "\n"
+        clear_remote(mirror, options)
       end
     end
   end

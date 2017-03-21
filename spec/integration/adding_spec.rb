1868f8538b04efe590e6b9730728f80dfc999658
Peter Donald <peter@realityforge.org>
2017-03-21 22:43:15 +1100

    Rework integration tests so that the library does not need to be loaded to run the tests

--- spec/integration/adding_spec.rb
+++ spec/integration/adding_spec.rb
@@ -31,7 +31,7 @@ describe 'Adding a mirror in a clean repository' do
         run_command("#{BRAID_BIN} add #{@skit1}")
       end
 
-      braids = YAML::load_file("#{@shiny}/#{Braid::CONFIG_FILE}")
+      braids = YAML::load_file("#{@shiny}/.braids.json")
       braids['skit1']['squashed'].should == true
       braids['skit1']['url'].should == @skit1
       braids['skit1']['revision'].should_not be_nil

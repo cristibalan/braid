bcc3d147fc8108ccc3f35e84b4d671e4fbb1273e
Peter Donald <peter@realityforge.org>
2017-03-23 09:37:43 +1100

    Simplify code

--- spec/integration/adding_spec.rb
+++ spec/integration/adding_spec.rb
@@ -11,13 +11,13 @@ describe 'Adding a mirror in a clean repository' do
     before do
       @repository_dir = create_git_repo_from_fixture('shiny', :name => 'Some body', :email => 'somebody@example.com')
       @vendor_repository_dir = create_git_repo_from_fixture('skit1')
-    end
 
-    it 'should add the files and commit' do
       in_dir(@repository_dir) do
         run_command("#{BRAID_BIN} add #{@vendor_repository_dir}")
       end
+    end
 
+    it 'should add the files and commit' do
       assert_no_diff("#{FIXTURE_PATH}/skit1/layouts/layout.liquid", "#{@repository_dir}/skit1/layouts/layout.liquid")
 
       assert_commit_subject(/Braid: Add mirror 'skit1' at '[0-9a-f]{7}'/)
@@ -26,10 +26,6 @@ describe 'Adding a mirror in a clean repository' do
     end
 
     it 'should create .braids.json and add the mirror to it' do
-      in_dir(@repository_dir) do
-        run_command("#{BRAID_BIN} add #{@vendor_repository_dir}")
-      end
-
       braids = YAML::load_file("#{@repository_dir}/.braids.json")
       expect(braids['skit1']['url']).to eq(@vendor_repository_dir)
       expect(braids['skit1']['revision']).not_to be_nil

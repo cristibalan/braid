96ad6c0a5a0983a9306d243607694e6c40d6dbd7
Peter Donald <peter@realityforge.org>
2017-03-23 17:29:56 +1100

    Add tests around pushing mirror with subdirectory

--- spec/integration/push_spec.rb
+++ spec/integration/push_spec.rb
@@ -99,4 +99,51 @@ describe 'Pushing to a mirror' do
       end
     end
   end
+
+  describe 'from a git repository braided into subdirectory' do
+    before do
+      @repository_dir = create_git_repo_from_fixture('shiny', :name => 'Some body', :email => 'somebody@example.com')
+      @vendor_repository_dir = create_git_repo_from_fixture('skit1')
+      @file_name = 'layouts/layout.liquid'
+
+      in_dir(@repository_dir) do
+        run_command("#{BRAID_BIN} add #{@vendor_repository_dir} --path layouts")
+      end
+
+      in_dir(@vendor_repository_dir) do
+        run_command('git config receive.denyCurrentBranch updateInstead')
+      end
+
+      update_dir_from_fixture('shiny/skit1', 'skit1.1/layouts')
+      in_dir(@repository_dir) do
+        run_command('git add *')
+        run_command('git commit -m "Make some changes to vendored files"')
+      end
+    end
+
+    context 'with remote updtodate' do
+      it 'should push changes successfully' do
+        braid_output = nil
+        commit_message = 'Make some changes'
+        in_dir(@repository_dir) do
+          set_editor_message(commit_message)
+          braid_output = run_command("#{EDITOR_CMD_PREFIX} #{BRAID_BIN} push skit1")
+        end
+        expect(braid_output).to match(/Braid: Cloning mirror with local changes./)
+        expect(braid_output).to match(/Make some changes/)
+        expect(braid_output).to match(/Braid: Pushing changes to remote branch master./)
+
+        assert_no_diff("#{FIXTURE_PATH}/skit1.1/#{@file_name}", "#{@repository_dir}/skit1/layout.liquid")
+        assert_no_diff("#{FIXTURE_PATH}/skit1.1/#{@file_name}", "#{@vendor_repository_dir}/#{@file_name}")
+
+        in_dir(@vendor_repository_dir) do
+          run_command('git checkout master 2>&1')
+
+          assert_commit_subject(commit_message)
+          assert_commit_author('Some body')
+          assert_commit_email('somebody@example.com')
+        end
+      end
+    end
+  end
 end

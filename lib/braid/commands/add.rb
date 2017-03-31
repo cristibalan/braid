e3ec1bbbdc0c63fc44b31fe7877c773f9b385ea9
Peter Donald <peter@realityforge.org>
2017-03-31 17:51:40 +1100

    Add some tests to verify add works with specific revisions

--- lib/braid/commands/add.rb
+++ lib/braid/commands/add.rb
@@ -6,15 +6,18 @@ module Braid
           mirror           = config.add_from_options(url, options)
           add_config_file
 
+          mirror.branch = nil if options['revision']
+          raise BraidError, 'Can not add mirror specifying both a revision and a tag' if options['revision'] && mirror.tag
+
           branch_message   = (mirror.branch.nil? || mirror.branch == 'master') ? '' : " branch '#{mirror.branch}'"
           tag_message      = mirror.tag.nil? ? '' : " tag '#{mirror.tag}'"
-          raise BraidError, 'Can not add mirror specifying both a revision and a tag' if options['revision'] && mirror.tag
           revision_message = options['revision'] ? " at #{display_revision(mirror, options['revision'])}" : ''
           msg "Adding mirror of '#{mirror.url}'#{branch_message}#{tag_message}#{revision_message}."
 
           # these commands are explained in the subtree merge guide
           # http://www.kernel.org/pub/software/scm/git/docs/howto/using-merge-subtree.html
 
+          config.update(mirror)
           setup_remote(mirror)
           mirror.fetch
 
--- spec/integration/adding_spec.rb
+++ spec/integration/adding_spec.rb
@@ -94,6 +94,38 @@ describe 'Adding a mirror in a clean repository' do
     end
   end
 
+  describe 'from a revision in a git repository' do
+    before do
+      @repository_dir = create_git_repo_from_fixture('shiny', :name => 'Some body', :email => 'somebody@example.com')
+      @vendor_repository_dir = create_git_repo_from_fixture('skit1')
+      in_dir(@vendor_repository_dir) do
+        run_command('git tag v1')
+        @revision = run_command('git rev-parse HEAD').strip
+      end
+
+      in_dir(@repository_dir) do
+        run_command("#{BRAID_BIN} add #{@vendor_repository_dir} --revision #{@revision}")
+      end
+    end
+
+    it 'should add the files and commit' do
+      assert_no_diff("#{FIXTURE_PATH}/skit1/layouts/layout.liquid", "#{@repository_dir}/skit1/layouts/layout.liquid")
+
+      assert_commit_subject(/Braid: Add mirror 'skit1' at '[0-9a-f]{7}'/)
+      assert_commit_author('Some body')
+      assert_commit_email('somebody@example.com')
+    end
+
+    it 'should create .braids.json and add the mirror to it' do
+      braids = YAML::load_file("#{@repository_dir}/.braids.json")
+      expect(braids['skit1']['url']).to eq(@vendor_repository_dir)
+      expect(braids['skit1']['revision']).not_to be_nil
+      expect(braids['skit1']['branch']).to be_nil
+      expect(braids['skit1']['tag']).to be_nil
+      expect(braids['skit1']['path']).to be_nil
+    end
+  end
+
   describe 'with a git repository' do
     before do
       @repository_dir = create_git_repo_from_fixture('shiny', :name => 'Some body', :email => 'somebody@example.com')

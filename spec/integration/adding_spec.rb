003bf428cc26c0807ddd51ed47d187bb013f5ad4
Stefan Felkel <stefan.felkel@besi.com>
2018-02-27 07:01:35 +0100

    test case: annotated tag

--- spec/integration/adding_spec.rb
+++ spec/integration/adding_spec.rb
@@ -138,6 +138,41 @@ describe 'Adding a mirror in a clean repository' do
     end
   end
 
+  describe 'from an annotated tag in a git repository' do
+    before do
+      @repository_dir = create_git_repo_from_fixture('shiny', :name => 'Some body', :email => 'somebody@example.com')
+      @vendor_repository_dir = create_git_repo_from_fixture('skit1')
+      in_dir(@vendor_repository_dir) do
+        run_command('git tag -a -m "v1" v1')
+      end
+
+      in_dir(@repository_dir) do
+        run_command("#{BRAID_BIN} add #{@vendor_repository_dir} --tag v1")
+      end
+    end
+
+    it 'should add the files and commit' do
+      assert_no_diff("#{FIXTURE_PATH}/skit1/layouts/layout.liquid", "#{@repository_dir}/skit1/layouts/layout.liquid")
+
+      in_dir(@repository_dir) do
+        assert_commit_subject(/Braid: Add mirror 'skit1' at '[0-9a-f]{7}'/)
+        assert_commit_author('Some body')
+        assert_commit_email('somebody@example.com')
+      end
+    end
+
+    it 'should create .braids.json and add the mirror to it' do
+      braids = YAML::load_file("#{@repository_dir}/.braids.json")
+      expect(braids['config_version']).to be_kind_of(Numeric)
+      mirror_obj = braids['mirrors']['skit1']
+      expect(mirror_obj['url']).to eq(@vendor_repository_dir)
+      expect(mirror_obj['revision']).not_to be_nil
+      expect(mirror_obj['branch']).to be_nil
+      expect(mirror_obj['tag']).to eq('v1')
+      expect(mirror_obj['path']).to be_nil
+    end
+  end
+
   describe 'from a revision in a git repository' do
     before do
       @repository_dir = create_git_repo_from_fixture('shiny', :name => 'Some body', :email => 'somebody@example.com')
